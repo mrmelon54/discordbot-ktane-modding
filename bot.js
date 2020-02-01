6964da24e5bbe06c9c0a10a01dd7ce3b39ce1602
Melons <melonmonster.54@gmail.com>
2020-02-01 19:57:26 +0000

    Clearing up

--- bot.js
+++ bot.js
@@ -2,8 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const config = require('./config.json');
-const server=require('./server.js');
-server.sendClient(client);
 
 var pastTopics=require('./pasttopics.json');
 var preparedStorageFileUpdate=false;
